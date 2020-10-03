8a2b79244fac6e8551e4a662ab20bd62c2722246
Joey <joeymchau@gmail.com>
2020-10-02 22:08:14 -0400

    remove extra axios from App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from 'react'
-import axios from 'axios';
 
 import UserInput from './UserInput';
 
 export default class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      data: []
-    }
-  }
-
 
   render() {
     return (
