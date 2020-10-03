01beaa719b3e1b9915658ea497e2a76069bba213
Aaron <aws.clemens@gmail.com>
2020-10-03 12:35:00 -0600

    fixed math for end time to include integers, not strings

--- src/UserInput.js
+++ src/UserInput.js
@@ -10,60 +10,67 @@ export default class UserInput extends Component {
             initialTime: "",
             duration: 0,
             endTime: "",
-            etcList: [],
-            timeZone: "",
+            timeZoneList: [],
+            intialTimeZone: "",
+            intialTimeZoneOffset: 0,
         }
     }
 
     componentDidMount() {
+        
         axios({
-            url: `http://worldtimeapi.org/api/timezone/Etc`,
+            url: `http://worldtimeapi.org/api/timezone/`,
 
         }).then(response => {
+            const allZones = response.data;
+            const timeZoneArr = allZones.filter((zone) => {
+                return(zone.startsWith("America") || 
+                zone.startsWith("Antarctica") || 
+                zone.startsWith("Asia") || 
+                zone.startsWith("Atlantic") || 
+                zone.startsWith("Australia") || 
+                zone.startsWith("Europe") || 
+                zone.startsWith("Indian") || 
+                zone.startsWith("Pacific"))
+            });
             this.setState({
-                etcList: response.data,
+                timeZoneList: timeZoneArr,
             })
         })
-
     }
 
     timeDropDownLoop = (start, end) => {
-        let arr = [];
+    let arr = [<option value={""}>{""}</option>];
         for (let i = start; i < end; i++) {
             let time = i;
             if (start === 8 && time > 12) time -= 12;
             arr.push(<option value={i}> {time} </option>)
         }
-
         return arr;
     }
 
-    etcDropDownLoop = () => {
+    ZoneDropDownLoop = () => {
 
-        const arr = this.state.etcList.map((timeZoneName) => {
-            return (<option value={timeZoneName}> {timeZoneName} </option >)
+        const arr = this.state.timeZoneList.map((timeZoneName) => {
+            return (<option value={timeZoneName}> {`${timeZoneName}`} </option >)
         })
-
-        arr.pop();
         return arr;
-
     }
 
     addOrSubtract = (change) => {
         const duration = this.state.duration + change;
         if (duration < 5 && duration > 0) this.setState(prevState => ({
             duration: prevState.duration + change,
-            endTime: prevState.initialTime + duration
+            endTime: parseInt(prevState.initialTime) + parseInt(duration)
         }))
     }
-
 
     render() {
         return (
             <div>
                 <form action="">
-                    <label htmlFor="">initialTime</label>
-                    <select value={this.state.initialTime} onChange={(e) => this.setState({ initialTime: e.target.value })} name="" id="">
+                    <label htmlFor="initialTime">initialTime</label>
+                    <select value={this.state.initialTime} onChange={(e) => this.setState({ initialTime: parseInt(e.target.value) })} name="" id="initialTime">
                         {this.timeDropDownLoop(8, 19)}
                     </select>
 
@@ -72,10 +79,11 @@ export default class UserInput extends Component {
                     <span>{this.state.duration}</span>
                     <i className="fas fa-plus" onClick={() => this.addOrSubtract(+1)}></i>
 
-                    <label htmlFor=""></label>
-                    <select value={this.state.timeZone} name="" id="" onChange={(e) => this.setState({ timeZone: e.target.value })}>
-                        {this.etcDropDownLoop()}
-                    </select>
+                    <label htmlFor="timeZone">Time Zone:</label>
+                    <input list="timeZones" name="timeZone" id="timeZone" value={this.state.intialTimeZone} onChange={(e) => this.setState({ intialTimeZone: e.target.value })}/>
+                    <datalist  id="timeZones" >
+                        {this.ZoneDropDownLoop()}
+                    </datalist>
                 </form>
             </div>
         );
